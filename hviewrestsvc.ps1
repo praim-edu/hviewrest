de162d20449e3c787315ea7d4c7ebc33a326f69d
jacopoferraro <jacopoferraro@praim.com>
2019-06-07 11:55:22 +0200

    Added entity field from querystring parameter

--- hviewrestsvc.ps1
+++ hviewrestsvc.ps1
@@ -1,3 +1,27 @@
+$queryableDataObjects = @(
+    "ADUserOrGroupSummaryView",
+    "ApplicationIconInfo",
+    "ApplicationInfo",
+    "DesktopSummaryView",
+    "EntitledUserOrGroupGlobalSummaryView",
+    "EntitledUserOrGroupLocalSummaryView",
+    "EventSummaryView",
+    "FarmHealthInfo",
+    "FarmSummaryView",
+    "GlobalApplicationEntitlementInfo",
+    "GlobalEntitlementSummaryView",
+    "MachineNamesView",
+    "MachineSummaryView",
+    "PersistentDiskInfo",
+    "PodAssignmentInfo",
+    "RDSServerInfo",
+    "RDSServerSummaryView",
+    "RegisteredPhysicalMachineInfo",
+    "SessionGlobalSummaryView",
+    "SessionLocalSummaryView",
+    "TaskInfo",
+    "UserHomeSiteInfo")
+
 function Get-HViewQueryResults
 {
     Param
@@ -6,13 +30,14 @@ function Get-HViewQueryResults
         [string]$user,
         [string]$password,
         [string]$domain,
+        [string]$entity,
         [string]$command,
         [string]$property,
         [string]$value
     )
     # Ignora errori certificati
     #Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -ParticipateInCeip $false
-    # Connessione al server hview locale
+    # Connessione al server hview
     $hvServer = Connect-HVServer -server $server -user $user -password $password -domain $domain
     # Popolo variabile servizi di query
     $hvServices = $hvServer.ExtensionData
@@ -20,13 +45,29 @@ function Get-HViewQueryResults
     $queryService = New-Object VMware.Hv.QueryServiceService
     # Nuova istanza oggetto query definition
     $queryDefinition = New-Object VMware.Hv.QueryDefinition
-    # Imposto su quale entità fare la query
-    $queryDefinition.queryEntityType = 'SessionLocalSummaryView'
+    # Controllo se entity fa parte dei valori possibili
+    $entity = $queryableDataObjects -match "$entity"
+    # Controllo che entity sia valida
+    if ($entity -eq $null -or $entity -eq "") {
+        return $null
+    }
+    # Imposto su quale entita fare la query
+    $queryDefinition.queryEntityType = $entity
     # Eseguo la query
-    $queryResults = $queryService.QueryService_Create($hvServices, $queryDefinition)
+    try {
+        $queryResults = $queryService.QueryService_Create($hvServices, $queryDefinition)
+        # Filtro i risultati della query
+        $result = foreach ($obj in $queryResults.Results) { $obj.$command }
+        $result2 = foreach ($obj2 in $result) { if ($obj2.$property -match $value) { $obj2 } }
+        # Elimino la query
+        $queryService.QueryService_Delete($hvServices, $queryResults.Id)
+    }
+    catch {
+        return $null
+    }
+    # Disconnessione dal server hview
+    #Disconnect-HVServer -Server $hvServer
     # Restituisco i risultati della query
-    $result = foreach ($obj in $queryResults.Results) { $obj.$command }
-    $result2 = foreach ($obj2 in $result) { if ($obj2.$property -match $value) { $obj2 } }
     return $result2
 }
 
@@ -52,18 +93,23 @@ while ($true) {
         # Split request URL to get command and options
         $requestvars = ([String]$request.Url).split("/");
 
-        # If a request is sent to http:// :8000/namesdata
-        if ($requestvars[3] -eq "namesdata" -or $requestvars[3] -eq "sessiondata") {
+        $server = $($args[0])
+        $user = $($args[1])
+        $password = $($args[2])
+        $domain = $($args[3])
+
+        if ($server -ne $null -and
+            $user -ne $null -and
+            $password -ne $null -and
+            $domain -ne $null -and
+            $requestvars[3] -ne $null -and
+            $requestvars[3] -ne "") {
             # Esegui la funzione Get-HViewQueryResults con argomento in ingresso
-            $result = Get-HViewQueryResults -server $($args[0]) -user $($args[1]) -password $($args[2]) -domain $($args[3]) -command $requestvars[3] -property $requestvars[4] -value $requestvars[5]
-            # Convert the returned data to JSON and set the HTTP content type to JSON
-            $message = ConvertTo-Json $result;
-            $response.ContentType = 'application/json';
-       } else {
-            # If no matching subdirectory/route is found generate a 404 message
-            $message = "This is not the page you're looking for.";
-            $response.ContentType = 'text/html';
-       }
+            $result = Get-HViewQueryResults -server $server -user $user -password $password -domain $domain -entity $requestvars[3] -command $requestvars[4] -property $requestvars[5] -value $requestvars[6] 
+        }
+        # Convert the returned data to JSON and set the HTTP content type to JSON
+        $message = ConvertTo-Json $result;
+        $response.ContentType = 'application/json';
 
        # Return empty message if message is null
        if ($message -eq $null) { $message = "" }
