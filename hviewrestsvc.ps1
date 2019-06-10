6009ddc7c6f50e62a96885afb2815269eb45f17b
jacopoferraro <jacopoferraro@praim.com>
2019-06-10 15:00:41 +0200

    Added cpu, ram and disk counters

--- hviewrestsvc.ps1
+++ hviewrestsvc.ps1
@@ -35,40 +35,68 @@ function Get-HViewQueryResults
         [string]$property,
         [string]$value
     )
-    # Ignora errori certificati
-    #Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -ParticipateInCeip $false
-    # Connessione al server hview
-    $hvServer = Connect-HVServer -server $server -user $user -password $password -domain $domain
-    # Popolo variabile servizi di query
-    $hvServices = $hvServer.ExtensionData
-    # Nuova istanza oggetto servizio query
-    $queryService = New-Object VMware.Hv.QueryServiceService
-    # Nuova istanza oggetto query definition
-    $queryDefinition = New-Object VMware.Hv.QueryDefinition
-    # Controllo se entity fa parte dei valori possibili
-    $entity = $queryableDataObjects -match "$entity"
-    # Controllo che entity sia valida
-    if ($entity -eq $null -or $entity -eq "") {
-        return $null
+    #Get CPU usage from system
+    if ($entity -eq  "cpu") {
+         $CpuLoad = (Get-WmiObject win32_processor | Measure-Object -property LoadPercentage -Average | Select Average ).Average
+         return $CpuLoad
     }
-    # Imposto su quale entita fare la query
-    $queryDefinition.queryEntityType = $entity
-    # Eseguo la query
-    try {
-        $queryResults = $queryService.QueryService_Create($hvServices, $queryDefinition)
-        # Filtro i risultati della query
-        $result = foreach ($obj in $queryResults.Results) { $obj.$command }
-        $result2 = foreach ($obj2 in $result) { if ($obj2.$property -match $value) { $obj2 } }
-        # Elimino la query
-        $queryService.QueryService_Delete($hvServices, $queryResults.Id)
+    #Get ram usage from system
+    elseif ($entity -match "ram") {
+         $os = Get-Ciminstance Win32_OperatingSystem
+         $AvaregeRam = [math]::Round(($os.FreePhysicalMemory/$os.TotalVisibleMemorySize)*100,2)
+         $AvaregeRam = 100 - $AvaregeRam
+         return $AvaregeRam
     }
-    catch {
-        return $null
+    #Get disk usage
+    elseif ($entity -match "diskusage") {
+         $disk = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'" | Select-Object Size,FreeSpace
+         #trovo lo spazio utilizzato
+         $diskUse = $disk.Size/1MB - $disk.FreeSpace/1MB
+         return $diskUse
     }
-    # Disconnessione dal server hview
-    #Disconnect-HVServer -Server $hvServer
-    # Restituisco i risultati della query
-    return $result2
+    #Get disk io
+    elseif ($entity -match "diskio") {
+         $disk = Get-Counter -Counter "\PhysicalDisk(0 C:)\Disk Transfers/sec"
+         #numero di operazioni per secondo
+         $diskIO = $disk.CounterSamples[0].CookedValue
+         return $diskIO
+    } else {
+        # Ignora errori certificati
+        #Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -ParticipateInCeip $false
+        # Connessione al server hview
+        $hvServer = Connect-HVServer -server $server -user $user -password $password -domain $domain
+        # Popolo variabile servizi di query
+        $hvServices = $hvServer.ExtensionData
+        # Nuova istanza oggetto servizio query
+        $queryService = New-Object VMware.Hv.QueryServiceService
+        # Nuova istanza oggetto query definition
+        $queryDefinition = New-Object VMware.Hv.QueryDefinition
+        # Controllo se entity fa parte dei valori possibili
+        $entity = $queryableDataObjects -match "$entity"
+        # Controllo che entity sia valida
+        if ($entity -eq $null -or $entity -eq "") {
+            return $null
+        }
+        # Imposto su quale entita fare la query
+        $queryDefinition.queryEntityType = $entity
+        # Eseguo la query   
+        try {
+            $queryResults = $queryService.QueryService_Create($hvServices, $queryDefinition)
+            # Filtro i risultati della query
+            $result = foreach ($obj in $queryResults.Results) { $obj.$command }
+            $result2 = foreach ($obj2 in $result) { if ($obj2.$property -match $value) { $obj2 } }
+            # Elimino la query
+            $queryService.QueryService_Delete($hvServices, $queryResults.Id)
+        }
+        catch {
+            return $null
+        }
+        finally {
+            # Disconnessione dal server hview
+            #Disconnect-HVServer -Server $hvServer
+        }
+        return $result2
+    }
 }
 
 # Create a listener on port 8000
@@ -111,19 +139,19 @@ while ($true) {
         $message = ConvertTo-Json $result;
         $response.ContentType = 'application/json';
 
-       # Return empty message if message is null
-       if ($message -eq $null) { $message = "" }
+        # Return empty message if message is null
+        if ($message -eq $null) { $message = "" }
 
-       # Convert the data to UTF8 bytes
-       [byte[]]$buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
+        # Convert the data to UTF8 bytes
+        [byte[]]$buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
        
-       # Set length of response
-       $response.ContentLength64 = $buffer.length
-       
-       # Write response out and close
-       $output = $response.OutputStream
-       $output.Write($buffer, 0, $buffer.length)
-       $output.Close()
+        # Set length of response
+        $response.ContentLength64 = $buffer.length
+
+        # Write response out and close
+        $output = $response.OutputStream
+        $output.Write($buffer, 0, $buffer.length)
+        $output.Close()
     }
 }
 
